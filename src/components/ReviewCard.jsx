5b6c2c3b29334fc955ff0c3cc98adbc90b1fb1f0
Gabriele Bonucci <g.bonucci99@gmail.com>
2025-11-04 18:00:45 +0100

    Milestone 4: Aggiunta DetailPage, carica film singolo e mostra recensioni

---
+++ src/components/ReviewCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const ReviewCard = ({ reviewProp }) => {
+
+  const { author, text, vote } = reviewProp;
+
+  return (
+    <div className="card mb-3">
+      <div className="card-body">
+        <p className="card-text">{text}</p>
+        <footer className="blockquote-footer">
+          {author} - Voto: <cite title="Voto">{vote}/5</cite>
+        </footer>
+      </div>
+    </div>
+  )
+}
+
+export default ReviewCard
--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,12 +1,74 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Link, useParams, useNavigate } from 'react-router-dom'
+import ReviewCard from '../components/ReviewCard'
 
 const DetailPage = () => {
+
+  const navigate = useNavigate();
+
+  const [film, setFilm] = useState();
+
+  const { id } = useParams();
+
+  // funzione per la chiamata axios
+  const fetchSingleFilm = () => {
+    axios.get(`http://localhost:3000/movies/${id}`)
+      .then(response => {
+        setFilm(response.data);
+      })
+      .catch(error => {
+        console.error("Errore nel caricamento del film:", error);
+        // Se il film non esiste (404), reindirizziamo
+        if (error.response && error.response.status === 404) {
+          navigate('/404'); 
+        }
+      });
+  }
+
+  useEffect(fetchSingleFilm, [id]);
+
+  // Funzione di generazione istanze reviews
+  const renderReviews = () => {
+    return film?.reviews.map(review => {
+      return (
+        <ReviewCard reviewProp={review} key={review.id} />
+      )
+    })
+  }
   return (
     <div>
-      <h1>Dettaglio del Film</h1>
-      <p>Qui ci saranno i dati di un singolo film.</p>
+      <header className="row mb-4">
+        <div className="col-md-4">
+          <img 
+            src={`http://localhost:3000/${film?.image}`} 
+            className="img-fluid rounded" 
+            alt={film?.title} 
+          />
+        </div>
+        <div className="col-md-8">
+          <h1>{film?.title}</h1>
+          <p className="lead">{film?.abstract}</p>
+        </div>
+      </header>
+
+      {/* Sezione Recensioni*/}
+      <section id="reviews" className="mb-4">
+        <h3>Recensioni</h3>
+        {renderReviews()}
+        
+        {/* Messaggio se non ci sono recensioni */}
+        {film && film.reviews.length === 0 && (
+          <p>Non ci sono ancora recensioni per questo film.</p>
+        )}
+      </section>
+
+      {/* Footer della Pagina */}
+      <footer className="border-top pt-3">
+        <Link className="btn btn-secondary" to="/">
+          &larr; Torna alla Home
+        </Link>
+      </footer>
     </div>
   )
 }
